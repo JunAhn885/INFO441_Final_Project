e5ef105d36f88a88a14a61f42172987144fe15e3
xyx0826 <xyx0826@hotmail.com>
2022-11-28 23:59:52 -0800

    Implement route /api/user: Get the authenticated user

--- routes/api/controllers/users.js
+++ routes/api/controllers/users.js
@@ -2,12 +2,28 @@
 
 import express from "express";
 
-import { ApiError } from "../utils.js";
+import { Database } from "../utils.js";
 
 const router = express.Router();
 
+/**
+ * GET the authenticated user.
+ */
 router.get("/", async (req, res) => {
-  throw new ApiError("This is a custom error.");
+  // Get the user
+  const userPath = `/users/${req.user.id}`;
+  let user = await Database.get(userPath);
+  if (!user) {
+    // Our first time seeing this user, save their info
+    user = {
+      name: req.user.name,
+      email: req.user.email
+    };
+
+    await Database.set(userPath, user);
+  }
+
+  res.json(user);
 });
 
 export default router;
