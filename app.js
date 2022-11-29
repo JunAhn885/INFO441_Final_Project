9d7a10a28eb6d0a725faaff5e9fb5459805b9c9f
xyx0826 <xyx0826@hotmail.com>
2022-11-28 23:26:08 -0800

    Use a file-based session store in development to prevent silly cookie errors

--- app.js
+++ app.js
@@ -7,16 +7,19 @@ import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import "express-async-errors";
-import sessions from "express-session";
+import session from "express-session";
 import { initializeApp } from "firebase/app";
 import { equalTo, get, getDatabase, onValue, orderByChild, query, ref, set } from "firebase/database";
 import msIdExpress from "microsoft-identity-express";
 import logger from "morgan";
+import fileStore from "session-file-store";
 
 import apiRouter from "./routes/api/api.js";
 
+// Load environment variables
 dotenv.config();
 
+// Create setting objects
 const msalSettings = {
   appCredentials: {
     clientId: process.env.AZURE_CLIENT_ID,
@@ -43,8 +46,10 @@ const firebaseConfig = {
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Initialize Firebase Realtime Database
 initializeApp(firebaseConfig);
 
+// Initialize Express
 const app = express();
 
 app.use(logger("dev"));
@@ -53,14 +58,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Initialize Express session
 const oneDay = 1000 * 60 * 60 * 24;
-app.use(sessions({
-  secret: process.env.SESSION_SECRET,
-  saveUninitialized: true,
-  cookie: { maxAge: oneDay },
-  resave: false
-}));
+if (process.env.DEBUG) {
+  console.log("Using file session store");
+  const FileStore = fileStore(session);
+  app.use(session({
+    store: new FileStore({}),
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+  }));
+} else {
+  console.log("Using in-memory session store");
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false
+  }));
+}
 
+// Initialize Azure Auth
 const msid = new msIdExpress.WebAppAuthClientBuilder(msalSettings).build();
 app.use(msid.initialize());
 
@@ -80,6 +100,7 @@ app.get("/unauthorized", (req, res) => {
   res.status(401).send("Error: Unauthorized")
 });
 
+// Register API routes
 app.use("/api", apiRouter);
 
 export default app;
