2ccc4e503889b3fbfbb1f85279854d0a6d5fb72a
xyx0826 <xyx0826@hotmail.com>
2022-11-29 18:03:17 -0800

    Enable /api/user to get user with ID

--- routes/api/controllers/users.js
+++ routes/api/controllers/users.js
@@ -1,29 +1,44 @@
 "use strict";
 
 import express from "express";
-import { getDatabase, ref, set } from "firebase/database";
-import { Database } from "../utils.js";
+import { ApiError, Database } from "../utils.js";
 
 const router = express.Router();
 
 /**
- * GET the authenticated user.
+ * GET the authenticated user, or
+ * GET a user with the given ID.
  */
-router.get("/", async (req, res) => {
-  // Get the user
-  const userPath = `/users/${req.user.id}`;
-  let user = await Database.get(userPath);
-  if (!user) {
-    // Our first time seeing this user, save their info
-    user = {
-      name: req.user.name,
-      email: req.user.email
-    };
+router.get("/:id?", async (req, res) => {
+  let user = {};
 
-    await Database.set(userPath, user);
+  if (req.params.id) {
+    // Get another user
+    const userPath = `/users/${req.params.id}`;
+    user = await Database.get(userPath);
+    if (!user) {
+      throw new ApiError("Invalid user ID.");
+    }
+
+    user.id = req.params.id;
+  } else {
+    // Get the authenticated user
+    const userPath = `/users/${req.user.id}`;
+    user = await Database.get(userPath);
+    if (!user) {
+      // Our first time seeing this user, save their info
+      user = {
+        name: req.user.name,
+        email: req.user.email
+      };
+
+      await Database.set(userPath, user);
+      user.id = req.user.id;
+    }
+
+    user.id = req.user.id;
   }
 
-  user.id = req.user.id;
   res.json(user);
 });
 
