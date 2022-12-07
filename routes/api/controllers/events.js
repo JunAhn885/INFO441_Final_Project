1cd5e59e9e124c177c33854b071991d4a2f049d9
xyx0826 <xyx0826@hotmail.com>
2022-12-07 05:13:17 -0800

    Add feature to add events

--- routes/api/controllers/events.js
+++ routes/api/controllers/events.js
@@ -30,4 +30,40 @@ router.get("/:orgId", async (req, res) => {
   res.json(result);
 });
 
+router.post("/:orgId", async (req, res) => {
+  if (!await Database.hasTag(req.params.orgId, req.user.id, Tags.Owner)) {
+    throw new ApiError(
+      "Not authorized to create events in the given organization."
+    );
+  }
+
+  if (!req.body.name) {
+    throw new ApiError("Must specify a name for the new event.");
+  }
+
+  const timeNotBefore = new Date(req.body.timeNotBefore);
+  if (!isFinite(timeNotBefore)) {
+    throw new ApiError("Invalid start date time.")
+  }
+
+  const timeNotAfter = new Date(req.body.timeNotAfter);
+  if (!isFinite(timeNotAfter)) {
+    throw new ApiError("Invalid end date time.");
+  }
+
+  const event = {
+    creator: req.user.id,
+    name: req.body.name,
+    timeNotBefore: timeNotBefore.toISOString(),
+    timeNotAfter: timeNotAfter.toISOString(),
+    key: crypto.randomBytes(8).toString("hex")
+  };
+
+  const id = await Database.insert(`/orgs/${req.params.orgId}/events`, event);
+  res.json({
+    id: id,
+    ...event
+  });
+});
+
 export default router;
